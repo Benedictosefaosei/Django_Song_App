7905798c31accf9b6ee43713db7eb1da0ddb2c1f
benedictosefaosei <benedictosefaosei@gmail.com>
2026-03-08 06:12:56 -0400

    style for registration and added a login using class based views to the application

--- website/settings.py
+++ website/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "music.apps.MusicConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "music.apps.MusicConfig",
-    "users.apps.UsersConfig"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
